adeab3139fd7dd515c9c53d97ae6500b0342e94f
Renato Priolo <rpriolo@gmail.com>
2023-09-25 14:31:05 -0300

    Refatoração do banco de dados

--- src/controller/database/BancoDeDados.java
+++ src/controller/database/BancoDeDados.java
@@ -1,2 +1,51 @@
+package controller.database;
+
+import domain.*;
+import view.Main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BancoDeDados {
+    public static void gerarBancoDeDados() {
+        // SIMULANDO BANCO DE DADOS
+
+        List<Produto> carrinho = new ArrayList<Produto>();
+        List<Venda> vendas = new ArrayList<Venda>();
+
+        Empresa empresa = new Empresa(2, "SafeWay Padaria", "30021423000159", 0.15, 0.0);
+        Empresa empresa2 = new Empresa(1, "Level Varejo", "53239160000154", 0.05, 0.0);
+        Empresa empresa3 = new Empresa(3, "SafeWay Restaurante", "41361511000116", 0.20, 0.0);
+
+        Produto produto = new Produto(1, "Pão Frances", 5, 3.50, empresa);
+        Produto produto2 = new Produto(2, "Coturno", 10, 400.0, empresa2);
+        Produto produto3 = new Produto(3, "Jaqueta Jeans", 15, 150.0, empresa2);
+        Produto produto4 = new Produto(4, "Calça Sarja", 15, 150.0, empresa2);
+        Produto produto5 = new Produto(5, "Prato feito - Frango", 10, 25.0, empresa3);
+        Produto produto6 = new Produto(6, "Prato feito - Carne", 10, 25.0, empresa3);
+        Produto produto7 = new Produto(7, "Suco Natural", 30, 10.0, empresa3);
+        Produto produto8 = new Produto(8, "Sonho", 5, 8.50, empresa);
+        Produto produto9 = new Produto(9, "Croissant", 7, 6.50, empresa);
+        Produto produto10 = new Produto(10, "Ché Gelado", 4, 5.50, empresa);
+
+        Cliente cliente = new Cliente("07221134049", "Allan da Silva", "cliente", 20);
+        Cliente cliente2 = new Cliente("72840700050", "Samuel da Silva", "cliente2", 24);
+
+        Usuario usuario1 = new Usuario("admin", "1234", null, null);
+        Usuario usuario2 = new Usuario("empresa", "1234", null, empresa);
+        Usuario usuario3 = new Usuario("cliente", "1234", cliente, null);
+        Usuario usuario4 = new Usuario("cliente2", "1234", cliente2, null);
+        Usuario usuario5 = new Usuario("empresa2", "1234", null, empresa2);
+        Usuario usuario6 = new Usuario("empresa3", "1234", null, empresa3);
+
+
+        List<Usuario> usuarios = Arrays.asList(usuario1, usuario2, usuario3, usuario4, usuario5, usuario6);
+        List<Cliente> clientes = Arrays.asList(cliente, cliente2);
+        List<Empresa> empresas = Arrays.asList(empresa, empresa2, empresa3);
+        List<Produto> produtos = Arrays.asList(produto, produto2, produto3, produto4, produto5, produto6, produto7,
+                produto8, produto9, produto10);
+
+        Main.executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,8 @@
 package view;
 
+import controller.database.BancoDeDados;
+import domain.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,44 +12,7 @@ import java.util.stream.Collectors;
 public class Main {
 
 	public static void main(String[] args) {
-
-		// SIMULANDO BANCO DE DADOS
-
-		List<Produto> carrinho = new ArrayList<Produto>();
-		List<Venda> vendas = new ArrayList<Venda>();
-
-		Empresa empresa = new Empresa(2, "SafeWay Padaria", "30021423000159", 0.15, 0.0);
-		Empresa empresa2 = new Empresa(1, "Level Varejo", "53239160000154", 0.05, 0.0);
-		Empresa empresa3 = new Empresa(3, "SafeWay Restaurante", "41361511000116", 0.20, 0.0);
-
-		Produto produto = new Produto(1, "Pão Frances", 5, 3.50, empresa);
-		Produto produto2 = new Produto(2, "Coturno", 10, 400.0, empresa2);
-		Produto produto3 = new Produto(3, "Jaqueta Jeans", 15, 150.0, empresa2);
-		Produto produto4 = new Produto(4, "Calça Sarja", 15, 150.0, empresa2);
-		Produto produto5 = new Produto(5, "Prato feito - Frango", 10, 25.0, empresa3);
-		Produto produto6 = new Produto(6, "Prato feito - Carne", 10, 25.0, empresa3);
-		Produto produto7 = new Produto(7, "Suco Natural", 30, 10.0, empresa3);
-		Produto produto8 = new Produto(8, "Sonho", 5, 8.50, empresa);
-		Produto produto9 = new Produto(9, "Croissant", 7, 6.50, empresa);
-		Produto produto10 = new Produto(10, "Ché Gelado", 4, 5.50, empresa);
-
-		Cliente cliente = new Cliente("07221134049", "Allan da Silva", "cliente", 20);
-		Cliente cliente2 = new Cliente("72840700050", "Samuel da Silva", "cliente2", 24);
-
-		Usuario usuario1 = new Usuario("admin", "1234", null, null);
-		Usuario usuario2 = new Usuario("empresa", "1234", null, empresa);
-		Usuario usuario3 = new Usuario("cliente", "1234", cliente, null);
-		Usuario usuario4 = new Usuario("cliente2", "1234", cliente2, null);
-		Usuario usuario5 = new Usuario("empresa2", "1234", null, empresa2);
-		Usuario usuario6 = new Usuario("empresa3", "1234", null, empresa3);
-
-
-		List<Usuario> usuarios = Arrays.asList(usuario1, usuario2, usuario3, usuario4, usuario5, usuario6);
-		List<Cliente> clientes = Arrays.asList(cliente, cliente2);
-		List<Empresa> empresas = Arrays.asList(empresa, empresa2, empresa3);
-		List<Produto> produtos = Arrays.asList(produto, produto2, produto3, produto4, produto5, produto6, produto7,
-				produto8, produto9, produto10);
-		executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+		BancoDeDados.gerarBancoDeDados();
 	}
 
 	public static void executar(List<Usuario> usuarios, List<Cliente> clientes, List<Empresa> empresas,
@@ -81,12 +47,12 @@ public class Main {
 						vendas.stream().forEach(venda -> {
 							if (venda.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
 								System.out.println("************************************************************");
-								System.out.println("Venda de código: " + venda.getCódigo() + " no CPF "
+								System.out.println("domain.Venda de código: " + venda.getCódigo() + " no CPF "
 										+ venda.getCliente().getCpf() + ": ");
 								venda.getItens().stream().forEach(x -> {
 									System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
 								});
-								System.out.println("Total Venda: R$" + venda.getValor());
+								System.out.println("Total domain.Venda: R$" + venda.getValor());
 								System.out.println("Total Taxa a ser paga: R$" + venda.getComissaoSistema());
 								System.out.println("Total Líquido  para empresa"
 										+ (venda.getValor() - venda.getComissaoSistema()));
@@ -94,7 +60,7 @@ public class Main {
 							}
 
 						});
-						System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
+						System.out.println("Saldo domain.Empresa: " + usuarioLogado.getEmpresa().getSaldo());
 						System.out.println("************************************************************");
 
 						executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
@@ -107,14 +73,14 @@ public class Main {
 							if (produto.getEmpresa().getId().equals(usuarioLogado.getEmpresa().getId())) {
 								System.out.println("************************************************************");
 								System.out.println("Código: " + produto.getId());
-								System.out.println("Produto: " + produto.getNome());
+								System.out.println("domain.Produto: " + produto.getNome());
 								System.out.println("Quantidade em estoque: " + produto.getQuantidade());
 								System.out.println("Valor: R$" + produto.getPreco());								
 								System.out.println("************************************************************");
 							}
 
 						});
-						System.out.println("Saldo Empresa: " + usuarioLogado.getEmpresa().getSaldo());
+						System.out.println("Saldo domain.Empresa: " + usuarioLogado.getEmpresa().getSaldo());
 						System.out.println("************************************************************");
 
 						executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
